5960b6528b3a9e196721b745b3bc283a2ad9619d
anastasiadergaeva <adergaeva@gmail.com>
2023-05-12 12:57:26 +0300

    final slider

---
+++ 2/index.js
@@ -0,0 +1,18 @@
+let offset = 0; // смещение от левого края
+const sliderImg = document.querySelector('.slider__img')
+
+document.querySelector('.btn__prev').addEventListener('click', function(){
+    offset -= 564;
+    if (offset < 0) {
+        offset = 1128;
+    }
+    sliderImg.style.left = -offset + 'px';
+});
+
+document.querySelector('.btn__next').addEventListener('click', function(){
+    offset += 564;
+    if (offset > 1128) {
+        offset = 0;
+    }
+    sliderImg.style.left = -offset + 'px';
+});
